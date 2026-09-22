f63e7822b7534f752aa59d028eca6b5561bfd610
nroj95 <236590761+nroj95@users.noreply.github.com>
2026-09-22 15:20:44 +0200

    reorder handbook topics into loose groups

--- scripts/dst101data.lua
+++ scripts/dst101data.lua
@@ -1,28 +1,39 @@
 local TOPIC_MODULES = {
+    -- getting started
     "dst101topics/setting_out",
+    "dst101topics/exploration",
     "dst101topics/darkness",
     "dst101topics/fire",
     "dst101topics/food",
     "dst101topics/health",
     "dst101topics/death",
     "dst101topics/sanity",
+
+    -- making and keeping things
+    "dst101topics/gathering",
     "dst101topics/crafting",
     "dst101topics/camp",
     "dst101topics/storage",
     "dst101topics/equipment",
-    "dst101topics/gathering",
+
+    -- getting food and using the world
     "dst101topics/farming",
     "dst101topics/fishing",
     "dst101topics/hunting",
-    "dst101topics/exploration",
-    "dst101topics/sailing",
+
+    -- living things and danger
     "dst101topics/creatures",
     "dst101topics/companions",
     "dst101topics/combat",
     "dst101topics/hounds",
+
+    -- changing conditions
+    "dst101topics/seasons",
     "dst101topics/temperature",
     "dst101topics/wetness",
-    "dst101topics/seasons",
+
+    -- larger expeditions
+    "dst101topics/sailing",
     "dst101topics/beefalo",
     "dst101topics/caves",
 }
